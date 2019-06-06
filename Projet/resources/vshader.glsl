cf81e18935a815f4cf4986f90129badb5d2aa612
Guillaume Baldi <guillaume.baldi@utbm.fr>
2019-06-06 21:26:04 +0200

    Drawing model + Begin of animation

--- Projet/resources/vshader.glsl
+++ Projet/resources/vshader.glsl
@@ -1,25 +1,38 @@
 #version 330
 
+const int BONES = 19;
+const int WEIGHTS = 5;
+
 uniform mat4 mvp;
+uniform mat4 transform[BONES];
 
 in vec3 position;
 in vec3 color;
 in vec3 normal;
 in vec2 uv;
-in int id;
+in vec4 idBones;
+in vec4 weight;
+//in int[WEIGHTS] idBones;
+//in int[WEIGHTS] weight;
 
 out vec3 fColor;
 out vec2 fUv;
 
 void main()
 {
-	if(id < 500) {
-		gl_Position = mvp * vec4( position, 2.0 );
+	vec4 vertexPos = vec4(position, 1.0);
+	vec4 finalPos = vec4(0.0);
+	for(int i = 0; i < WEIGHTS; i++) {
+		if(idBones[i] != -1) {
+			mat4 bt = transform[int(idBones[i])];
+			vec4 p = bt * vertexPos;
+			finalPos += p * weight[i];
+		}
 	}
-	else {
-		gl_Position = mvp * vec4( position, 1.0 );
-	}
-    
+	
+	gl_Position = mvp * finalPos;
+
+	
 
     fColor = color;
     fUv = uv;
